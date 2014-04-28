b2c919de4b5ef3c1fa09aaa2f2d6fc1a78c25623
Brad Anderson <banderso@nerdery.com>
2014-04-28 16:51:37 -0500

    Removing the old requirjs way of including modules since we're using html imports  * Lots of clean up

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,6 +18,6 @@ module.exports = function (grunt) {
     });
 
     grunt.loadNpmTasks('grunt-contrib-connect');
-    grunt.registerTask('default', [ 'connect']);
+    grunt.registerTask('default', [ 'connect' ]);
 
 };
